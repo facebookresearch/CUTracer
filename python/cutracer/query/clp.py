1a8bfc45b37e85a1636a2fdfb49f24ce93c56635
Xu Zhao <xzhao9@fb.com>
2026-02-11 08:55:55 -0800

    Add clp query

--- python/cutracer/query/clp.py
+++ python/cutracer/query/clp.py
@@ -1,4 +1,5 @@
 import yscope_clp_core
+from yscope_clp_core import ClpQuery
 
 from cutracer.query.reader import TraceReaderBase
 
@@ -12,12 +13,20 @@ class TraceReaderCLP(TraceReaderBase):
     Provides efficient iteration over trace records.
 
     Example:
-        >>> reader = TraceReader("trace.clp")
+        >>> reader = TraceReaderCLP("trace.clp")
         >>> for record in reader.iter_records():
         ...     print(record["sass"])
     """
     def __init__(self, file):
+        assert file.exists(), f"Non-exist clp archive file: {file.absolute()}"
+        self._archive = file
+
+    def _filter_expr_to_clp_query(self, filter_exprs: str) -> str:
         pass
 
     def iter_records(self, filter_exprs: str) -> Generator:
-        pass
+        clp_query: ClpQuery = self._filter_expr_to_clp_query(filter_exprs)
+        with yscope_clp_core.search_archive(self._archive. clp_query) as iter:
+            for next_record in iter:
+                yield next_record
+
