331e31916259aed3ce7617268f0aecdf162eac82
Lei Wang (Triton) <wlei@meta.com>
2026-03-02 17:27:22 -0800

    Add CLI runner with `trace` subcommand for buck2 run
    Summary:
    Add a `trace` subcommand to the CUTracer Python CLI, enabling trace collection via `buck2 run` instead of manually setting environment variables.
    
    Before:
    ```
    CUTRACER_INSTRUMENT=tma_trace CUDA_INJECTION64_PATH=/path/to/cutracer.so ./vectoradd
    ```
    
    After:
    ```
    buck2 run fbcode//triton/tools/CUTracer:cutracer -- trace --instrument=tma_trace -- ./vectoradd
    ```
    
    Reviewed By: FindHao
    
    Differential Revision: D94700399

--- python/cutracer/cli.py
+++ python/cutracer/cli.py
@@ -13,6 +13,7 @@ import click
 from cutracer.analyze.cli import analyze_command
 from cutracer.query.cli import query_command
 from cutracer.reduce.cli import reduce_command
+from cutracer.runner import trace_command
 from cutracer.validation.cli import compare_command, validate_command
 
 
@@ -48,6 +49,7 @@ main.add_command(analyze_command)
 main.add_command(compare_command)
 main.add_command(query_command)
 main.add_command(reduce_command)
+main.add_command(trace_command)
 main.add_command(validate_command)
 
 
